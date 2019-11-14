7402f9cce0c35af91f963503f09514cbb32ccdf2
fallin9044 <a897229673@vip.qq.com>
2019-11-14 19:29:00 +0800

    initial

---
+++ src/main/java/com/marine/trafficflow/TrafficflowApplication.java
@@ -0,0 +1,13 @@
+package com.marine.trafficflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrafficflowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/marine/trafficflow/TrafficflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marine.trafficflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
