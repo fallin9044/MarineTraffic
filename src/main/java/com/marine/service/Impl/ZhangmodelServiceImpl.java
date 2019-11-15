99065026cce5d375117209d774194f88cd10ce65
cuishuyang <2279160513@qq.com>
2019-11-15 14:02:27 +0800

    “接口定义”

---
+++ src/main/java/com/marine/service/Impl/ZhangmodelServiceImpl.java
@@ -0,0 +1,46 @@
+package com.marine.service.Impl;
+
+import com.marine.entity.PositionMeta;
+import com.marine.service.zhangmodelService;
+
+/**
+ * @Auther: cuishuyang
+ * @Date: 2019/11/15 10:08
+ * @Description:
+ */
+public class ZhangmodelServiceImpl implements zhangmodelService {
+    @Override
+    public Boolean movingAvailable(PositionMeta position, PositionMeta prePosition) {
+        return null;
+    }
+
+    @Override
+    public Boolean speedAvailable(PositionMeta position, PositionMeta prePosition) {
+        return null;
+    }
+
+    @Override
+    public Boolean driftAvailable(PositionMeta position, PositionMeta prePosition) {
+        return null;
+    }
+
+    @Override
+    public Boolean accelerationAvailable(PositionMeta position, PositionMeta prePosition) {
+        return null;
+    }
+
+    @Override
+    public Boolean rateAvailable(PositionMeta position, PositionMeta prePOsition) {
+        return null;
+    }
+
+    @Override
+    public Boolean PositionAvailable(PositionMeta position, PositionMeta prePosition) {
+        return null;
+    }
+
+    @Override
+    public void ZhangDataAvailable(String fileName) {
+
+    }
+}
---
+++ src/main/java/com/marine/service/segmentationService.java
@@ -0,0 +1,9 @@
+package com.marine.service;
+
+/**
+ * @Auther: cuishuyang
+ * @Date: 2019/11/15 09:41
+ * @Description:
+ */
+public interface segmentation {
+}
